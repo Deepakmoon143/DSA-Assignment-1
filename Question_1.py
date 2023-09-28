b6376c8b4a13a326cf789b9f3ae5db7e6b6d7d39
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-09-28 23:50:27 +0530

    Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -1,31 +1,27 @@
 def find_pairs_with_sum(arr, target_sum):
     pairs = []
-    seen = set()
+    num = set() 
 
-    for num in arr:
-        complement = target_sum - num
-        if complement in seen:
-            pairs.append((num, complement))
-        seen.add(num)
+    for i in arr:
+        difference = target_sum - i
+        if difference in num:
+            pairs.append((i, difference))
+        num.add(i)
 
     return pairs
 
 
-try:
-    input_array = input("Enter an integer array (comma-separated): ")
-    target_sum = int(input("Enter the target sum: "))
-    
-    input_array = input_array.split(',')
-    input_array = [int(x.strip()) for x in input_array]
+arr_str = input("Enter the array elements separated by spaces: ")
+target_sum = int(input("Enter the target sum: "))
 
-    pairs = find_pairs_with_sum(input_array, target_sum)
 
-    if len(pairs) > 0:
-        print("Pairs with the sum {} are:".format(target_sum))
-        for pair in pairs:
-            print(pair)
-    else:
-        print("No pairs found with the sum {}.".format(target_sum))
+arr = list(map(int, arr_str.split()))
 
-except ValueError:
-    print("Invalid input. Please enter a valid integer array and target sum.")
+
+result = find_pairs_with_sum(arr, target_sum)
+
+if result:
+    print("Pairs with sum", target_sum, "are:", result)
+else:
+    print("No pairs with sum", target_sum, "found in the array.")
+
